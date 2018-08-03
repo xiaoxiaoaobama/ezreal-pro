a0bdc039d05dca6ac3a699b9d7ca333c6e119e02
RRCOA\wangyouwen <329307469@qq.com>
2018-08-03 17:33:44 +0800

    自动化生成router配置

--- src/router/route.js
+++ src/router/route.js
@@ -3,14 +3,14 @@ import { getMenuData } from '../menu/menu'
 
 function getFinishRoute() {
   const routerData = {}
-  Object.keys(routerConfig).forEach((path) => {
+  Object.keys(initRoute).forEach((path) => {
     const pathRegexp = pathToRegexp(path)
     const menuKey = Object.keys(menuData).find(key => pathRegexp.test(`${key}`))
     let menuItem = {}
     if (menuKey) {
       menuItem = menuData[menuKey]
     }
-    let router = routerConfig[path]
+    let router = initRoute[path]
     router = {
       ...router,
       key: router.key || menuItem.key,
@@ -33,8 +33,36 @@ function getFlatMenuData(menus) {
   return keys
 }
 
+function getMatchRoutesPath(path, routerPaths) {
+  let routes = routerPaths.filter(
+    routePath => routePath.indexOf(path) === 0 && routePath !== path
+  )
+  return routes
+}
+
+function routerHandle(paths) {
+  let cRouter = []
+  let emRouter = []
+
+  for (let path of paths) {
+    if (path === '/' || emRouter.indexOf(path) > -1) continue
+
+    const currentRoute = routerConfig[path]
+    currentRoute.path = path
+    const matchPaths = getMatchRoutesPath(path, paths)
+
+    if (matchPaths && matchPaths.length > 0) {
+      emRouter = emRouter.concat(matchPaths)
+      currentRoute.children = routerHandle(matchPaths)
+    }
+    cRouter.push(currentRoute)
+  }
+
+  return cRouter
+}
+
 // 提供给用户的配置形式
-const routerConfig = { // eslint-disable-line
+const initRoute = {
   "/": {
     component: () => import('../layout/BasicLayout.vue'),
     meta: {},
@@ -62,53 +90,52 @@ const routerConfig = { // eslint-disable-line
   }
 }
 
-const menuData = getFlatMenuData(getMenuData())
-const aa = getFinishRoute()
-console.log(aa)
+const menuData = getFlatMenuData(getMenuData()) // 树形结构转换成平级
+const routerConfig = getFinishRoute() // eslint-disable-line
 
-// 最终产出的路由结构
-const routes = [{
+export default [{
+  ...routerConfig['/'],
   path: '/',
-  component: () => import('../layout/BasicLayout.vue'),
-  redirect: '/home',
-  meta: {},
-  children: [
-    {
-      path: 'dashboard',
-      redirect: 'dashboard/analysis'
-    },
-    {
-      path: 'search',
-      redirect: 'search/articles'
-    },
-
-    {
-      path: 'home',
-      component: () => import('../views/home/home.vue'),
-      meta: {}
-    },
-
-    {
-      path: 'dashboard/analysis',
-      component: () => import('../views/dashboard/analysis.vue'),
-      meta: {}
-    },
-
-    {
-      path: 'search',
-      component: () => import('../views/list/search.vue'),
-      meta: {},
-      children: [{
-        path: 'articles',
-        component: () => import('../views/list/articles.vue'),
-        meta: {}
-      }, {
-        path: 'projects',
-        component: () => import('../views/list/projects.vue'),
-        meta: {}
-      }]
-    }
-  ]
+  children: routerHandle(Object.keys(routerConfig))
 }]
 
-export default routes
+// 最终产出的路由结构
+// const routes = [{
+//   path: '/',
+//   component: () => import('../layout/BasicLayout.vue'),
+//   redirect: '/home',
+//   meta: {},
+//   children: [
+//     {
+//       path: 'dashboard',
+//       redirect: 'dashboard/analysis'
+//     },
+
+//     {
+//       path: '/home',
+//       component: () => import('../views/home/home.vue'),
+//       meta: {}
+//     },
+
+//     {
+//       path: '/dashboard/analysis',
+//       component: () => import('../views/dashboard/analysis.vue'),
+//       meta: {}
+//     },
+
+//     {
+//       path: '/search',
+//       component: () => import('../views/list/search.vue'),
+//       meta: {},
+//       children: [{
+//         path: '/search/articles',
+//         component: () => import('../views/list/articles.vue'),
+//         meta: {}
+//       }, {
+//         path: '/search/projects',
+//         component: () => import('../views/list/projects.vue'),
+//         meta: {}
+//       }]
+//     }
+//   ]
+// }]
